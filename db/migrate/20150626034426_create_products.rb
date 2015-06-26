64e3acc536d07118c73439521c3e8892cf6f06d2
Mike Pitre <michaelepitre@gmail.com>
2015-06-26 10:00:57 -0500

    tables finished, not migrated yet

--- db/migrate/20150626034426_create_products.rb
+++ db/migrate/20150626034426_create_products.rb
@@ -2,9 +2,9 @@ class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
       t.string :title, null: false
-      t.string :description, null:false
+      t.string :description
       t.integer :price, null: false
-      t.integer :inventory, null:false
+      t.integer :inventory, default: 0
 
       t.timestamps null: false
     end
--- db/migrate/20150626040112_create_product_orders.rb
+++ db/migrate/20150626040112_create_product_orders.rb
@@ -3,7 +3,7 @@ class CreateProductOrders < ActiveRecord::Migration
     create_table :product_orders do |t|
       t.belongs_to :order, index: true, foreign_key: true
       t.belongs_to :product, index: true, foreign_key: true
-      t.integer :quantity
+      t.integer :quantity, default: 0
 
       t.timestamps null: false
     end
