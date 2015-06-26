6e11ccb3ebf3eee942ef2ea82a370de8a02723f7
Mike Pitre <michaelepitre@gmail.com>
2015-06-26 13:48:02 -0500

    add bottom margin to image in app css file

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,13 @@ Rails.application.routes.draw do
   resources :orders
   resources :products
   resources :users
+
+  get  'login', to: 'sessions#new',    as: 'login'
+  post 'login', to: 'sessions#create', as: 'create_session'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
+  root 'products#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
